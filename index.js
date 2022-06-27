37f8aec7eb7ca2354ffd7e614546027fd27c9613
rochafialvin <rochafi.dev@gmail.com>
2022-06-27 10:23:04 +0700

    Init

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+const port = 2104;
+
+app.get("/", (req, res) => {
+  res.send("API JALAN KOK");
+});
+
+app.get("/users", (req, res) => {
+  res.send({
+    status: "Success",
+    data: ["Hendrickson", "Thomspon", "Topson"],
+  });
+});
+
+app.listen(port, (error) => {
+  if (error) return console.log({ err: error.message });
+  console.log(`API berhasil running di port ${port}`);
+});
