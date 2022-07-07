e18b7c111cb5ea7ea7f30bde442c2dd7fac7a494
rochafialvin <rochafi.dev@gmail.com>
2022-07-07 12:22:52 +0700

    Setup pagination

--- src/routers/product/get.product.js
+++ src/routers/product/get.product.js
@@ -1,13 +1,46 @@
 const express = require("express");
+const { isFieldEmpties } = require("../../helpers");
 const router = express.Router();
 const { auth } = require("../../helpers/auth");
 const pool = require("../../lib/database");
 
 const getProductListController = async (req, res, next) => {
   try {
+    let { page, pageSize } = req.query;
+
+    const emptyFields = isFieldEmpties({ page, pageSize });
+
+    if (emptyFields.length) {
+      throw {
+        code: 400,
+        message: `Empty fields :  ${emptyFields}`,
+        data: { result: emptyFields },
+      };
+    }
+
+    // page 1
+    // pagesize : 6
+    // product : 1 - 6
+    // offset : 0 * 6 = 0 (tidak ada product yang dilewati)
+
+    // page 2
+    // pagesize : 6
+    // product : 7 - 12
+    // offset : 1 * 6 = 6 (6 product yang di lewati)
+
+    page = +page;
+    pageSize = +pageSize;
+
+    const limit = pageSize;
+    const offset = (page - 1) * pageSize;
+
     const connection = pool.promise();
-    const sqlGetProducts = `SELECT product_id, variant, price, origin FROM product`;
-    const [resGetProducts] = await connection.query(sqlGetProducts);
+    const sqlGetProducts = `SELECT product_id, variant, price, origin FROM product LIMIT ? OFFSET ?`;
+    const dataGetProducts = [limit, offset];
+    const [resGetProducts] = await connection.query(
+      sqlGetProducts,
+      dataGetProducts
+    );
 
     if (!resGetProducts.length) throw { message: "Product not found" };
 
