bdd44933280d29302f910fa82c4a9b3d7bbfa959
rochafialvin <rochafi.dev@gmail.com>
2022-07-08 10:49:16 +0700

    Create endpoint : Create cart

--- src/routers/cart/post.cart.js
+++ src/routers/cart/post.cart.js
@@ -6,7 +6,35 @@ const pool = require("../../lib/database");
 
 const createCartController = async (req, res, next) => {
   try {
-    res.send("Endpoint create cart is ready");
+    const { user_id } = req.user;
+    const { product_id, quantity } = req.body;
+
+    const emptyFields = isFieldEmpties({ product_id, quantity });
+
+    if (emptyFields.length) {
+      throw {
+        code: 400,
+        message: `Empty fields :  ${emptyFields}`,
+        data: { result: emptyFields },
+      };
+    }
+
+    const connection = pool.promise();
+    const sqlCreateCart = `INSERT INTO cart SET ?`;
+    const dataCreateCart = [{ user_id, product_id, quantity }];
+    const [resCreateCart] = await connection.query(
+      sqlCreateCart,
+      dataCreateCart
+    );
+
+    // affectedRows akan menyimpan integer yang menunjukkan jumlah data baru yang tergenerate
+    if (!resCreateCart.affectedRows)
+      throw { message: "Failed create new cart" };
+
+    res.status(201).send({
+      status: "Success",
+      message: "Success create new cart",
+    });
   } catch (error) {
     next(error);
   }
