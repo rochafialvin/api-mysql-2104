e812df55954a068578bef766156ccc490ee849b4
rochafialvin <rochafi.dev@gmail.com>
2022-07-08 16:02:18 +0700

    Create endpoint : Create transaction

--- src/routers/transaction/post.transaction.js
+++ src/routers/transaction/post.transaction.js
@@ -5,10 +5,71 @@ const { auth } = require("../../helpers/auth");
 const pool = require("../../lib/database");
 
 const createTransactionController = async (req, res, next) => {
+  let connection;
   try {
-    res.status(201).send("Create transaction endpoint is ready");
+    const { user_id } = req.user;
+    const { total, products } = req.body;
+
+    // products : [ {}, {} ] -->  mappedData : [ [], [] ]
+
+    connection = await pool.promise().getConnection();
+
+    await connection.beginTransaction();
+
+    // insert to transaction
+    const sqlCreateTransaction = `INSERT INTO transaction SET ?`;
+    const dataCreateTransaction = [{ user_id, total }];
+    const [resCreateTransaction] = await connection.query(
+      sqlCreateTransaction,
+      dataCreateTransaction
+    );
+
+    if (!resCreateTransaction.affectedRows)
+      throw { message: "Failed create transaction" };
+
+    // insert to detail transactions
+    const sqlCreateDetailTransaction = `INSERT INTO detailTransaction (transaction_id, product_id, product_name, product_price, product_description, product_origin,product_image,quantity) VALUES ?`;
+    const mappedData = products.map((product) => {
+      const {
+        product_id,
+        product_name,
+        product_price,
+        product_description,
+        product_origin,
+        product_image,
+        quantity,
+      } = product;
+      return [
+        resCreateTransaction.insertId,
+        product_id,
+        product_name,
+        product_price,
+        product_description,
+        product_origin,
+        product_image,
+        quantity,
+      ];
+    });
+    const dataCreateDetailTransaction = [mappedData];
+    const [resCreateDetailTransaction] = await connection.query(
+      sqlCreateDetailTransaction,
+      dataCreateDetailTransaction
+    );
+
+    if (!resCreateDetailTransaction.affectedRows)
+      throw { message: "Failed create detail transaction" };
+
+    await connection.commit();
+
+    res.status(201).send({
+      status: "Success",
+      message: "Success create transaction",
+    });
   } catch (error) {
+    connection && connection.rollback();
     next(error);
+  } finally {
+    connection && connection.release(); // menunggu penjelasan mengenai connection pool
   }
 };
 
