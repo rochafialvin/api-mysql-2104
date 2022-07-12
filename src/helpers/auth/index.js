6ed861d2f431c334f0ca02399e0b43c08d62cc40
rochafialvin <rochafi.dev@gmail.com>
2022-07-12 10:18:56 +0700

    Add more fields on get user

--- src/helpers/auth/index.js
+++ src/helpers/auth/index.js
@@ -11,14 +11,13 @@ const auth = async (req, res, next) => {
 
     const connection = pool.promise();
 
-    const sqlGetUser = `SELECT user_id, username FROM user WHERE user_id = ?`;
+    const sqlGetUser = `SELECT user_id, username, first_name, last_name, email, phone, age   FROM user WHERE user_id = ?`;
     const dataGetUser = [verifiedToken.user_id];
     const [resGetUser] = await connection.query(sqlGetUser, dataGetUser);
 
     if (!resGetUser.length) throw { message: "User not found" };
 
-    const { user_id, username } = resGetUser[0];
-    req.user = { user_id, username };
+    req.user = resGetUser[0];
     next();
   } catch (error) {
     next(error);
