b24011ca2ec3299328d312ac97d2427d76839515
rochafialvin <rochafi.dev@gmail.com>
2022-07-12 10:19:24 +0700

    Refactor handlebars custom function

--- src/lib/handlebars/basic.js
+++ src/lib/handlebars/basic.js
@@ -3,13 +3,27 @@ const fs = require("fs");
 const appRoot = require("app-root-path");
 
 const createHtmlString = (data) => {
+  /*
+    const data = {
+      user: { user_id, username, first_name, last_name, email, phone },
+      products: resGetTransaction,
+    };
+  */
+
+  // templateData = {user_id, username, first_name, last_name, email, phone, fullName, products}
+  const templateData = {
+    ...data.user,
+    fullName: `${data.user.first_name} ${data.user.last_name}`,
+    products: data.products,
+  };
+
   const templatePath = `${appRoot}/src/lib/handlebars/template/test.html`;
 
   const htmlSource = fs.readFileSync(templatePath, "utf-8");
 
   const htmlTemplate = handlebars.compile(htmlSource);
 
-  const htmlString = htmlTemplate(data);
+  const htmlString = htmlTemplate(templateData);
 
   return htmlString;
 };
